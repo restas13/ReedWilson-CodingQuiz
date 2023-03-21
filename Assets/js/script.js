40f91a80631eeca7178b7cdd24c028b1276717eb
restas13 <72560360+restas13@users.noreply.github.com>
2023-03-20 19:03:38 -0700

    Timer Timer now functions and calls a function when it reaches 0. buttons can now change text.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,15 @@
 var question = document.querySelector('#question');
-var choices = Array.from(document.querySelectorAll('.choice'));
+var choice1 = document.querySelector('#choice1');
+var choice2 = document.querySelector('#choice2');
+var choice3 = document.querySelector('#choice3');
+var choice4 = document.querySelector('#choice4');
 var progressText = document.querySelector('#progressText');
 var scoreText = document.querySelector('#score');
 
-var currentQuestion = {};
+var currentQuestion = 0;
 var acceptingAnswers = true;
 var score = 100;
+var finalScore = 0;
 var questionCount = 0;
 var availableQuestions = [];
 
@@ -16,7 +20,7 @@ var questions = [
         choice2: 'C#', 
         choice3: 'javascript', 
         choice4: 'html',
-        answer: 2,
+        answer: 'css',
     },
     {
         question: 'Commonly used data types do not include.', 
@@ -24,7 +28,7 @@ var questions = [
         choice2: 'string', 
         choice3: 'integer', 
         choice4: 'hypertext',
-        answer: 4,
+        answer: 'hypertext',
     },
     {
         question: 'An if statement should be followed by which of the following?', 
@@ -32,7 +36,7 @@ var questions = [
         choice2: '[]', 
         choice3: '==', 
         choice4: '-',
-        answer: 1,
+        answer: '()',
     },
     {
         question: 'what is 2+2?', 
@@ -40,7 +44,7 @@ var questions = [
         choice2: '5', 
         choice3: '4', 
         choice4: '6',
-        answer: 3,
+        answer: '4',
     },
     {
         question: 'What is the capitol of Florida', 
@@ -48,20 +52,22 @@ var questions = [
         choice2: 'Miami', 
         choice3: 'Tampa', 
         choice4: 'Jacksonville',
-        answer: 1,
+        answer: 'Talahassee',
     },
 ]
 
-const TIME_DEDUCTION = 15;
-const MAX_Questions = 4;
+setInterval(myTimer, 1000);
 
-startGame = () => {
-    questionCount = 0;
-    score = 100;
-    availableQuestions = [...questions];
-    getNewQuestion();
+function myTimer() {
+    if (score == 0){
+        ending();
+    }
+
+    score--;
+    scoreText.textContent = score;
 }
 
-function getNewQuestion() {
-    
+
+function ending() {
+
 }
